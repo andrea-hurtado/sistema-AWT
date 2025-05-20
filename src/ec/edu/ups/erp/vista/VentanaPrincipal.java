2952ea1056daf71fe0579e9b8b48b595188ba901
Juliana Torres <julianatc21@gmail.com>
2025-05-20 09:49:08 -0500

    feat: creación de ventanas

---
+++ src/ec/edu/ups/erp/vista/VentanaPrincipal.java
@@ -0,0 +1,35 @@
+package ec.edu.ups.erp.vista;
+
+import java.awt.*;
+
+public class VentanaPrincipal extends Frame {
+    private Panel panelSesion;
+
+    private Button botonSecion;
+    private Button botonRegistarUsuario;
+    private Button botonsalir;
+
+    public VentanaPrincipal() {
+        setTitle("--Sistema AWT--");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+
+
+        panelSesion = new Panel();
+        panelSesion.setLayout(new FlowLayout(FlowLayout.CENTER));
+        add(panelSesion);
+
+        botonSecion= new Button ("Iniciar Sesión");
+        botonRegistarUsuario = new Button ("Registrar Usuario");
+        botonsalir = new Button ("Salir");
+
+        panelSesion.add(botonSecion);
+        panelSesion.add(botonRegistarUsuario);
+        panelSesion.add(botonsalir);
+
+        setVisible(true);
+
+    }
+
+
+}
