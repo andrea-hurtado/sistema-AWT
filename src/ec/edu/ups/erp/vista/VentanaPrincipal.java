cd591050dfdf080b1eb7b398df48b03568df739b
Andrea <2007andreahurtado@gmail.com>
2025-05-20 09:41:03 -0500

    feat: ventana para inicio de sesio, hasta salir.

---
+++ src/ec/edu/ups/erp/vista/VentanaPrincipal.java
@@ -0,0 +1,18 @@
+package ec.edu.ups.erp.vista;
+
+import java.awt.*;
+
+public class VentanaPrincipal extends Frame {
+    private Panel panelSesion;
+
+    private Button iniciarSesion;
+    private Button registrarSesion;
+    private Button salirSesion;
+
+    public VentanaPrincipal() {
+        setTitle("--SISTEMA AWT--");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+
+    }
+}
